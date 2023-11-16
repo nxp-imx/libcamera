dd032a7ac098cafd98e5dcd82a44c8e487247d36
Fang Hui <hui.fang@nxp.com>
2023-11-16 21:48:24 +0800

    update android-reconfig.sh
    Signed-off-by: Fang Hui <hui.fang@nxp.com>

--- android-reconfig.sh
+++ android-reconfig.sh
@@ -1,1 +1,11 @@
-~/.local/bin/meson build --cross-file android-meson.cross --sysconfdir /vendor/etc/configs -Dpipelines=imx8-isi -Dandroid=enabled -Dandroid_root=/home/fanghui/share_home2/android-14-imx95 --reconfigure
+ANDROID_ROOT=/home/fanghui/share_home2/android-14-imx95
+PRODUCT_PATH=$ANDROID_ROOT/out/target/product/evk_95
+NDK_LIB_PATH=./src/android/ndk-lib
+
+mkdir -p $NDK_LIB_PATH
+cp -f $PRODUCT_PATH/system/lib64/libexif.so $NDK_LIB_PATH
+cp -f $PRODUCT_PATH/system/lib64/libjpeg.so $NDK_LIB_PATH
+cp -f $PRODUCT_PATH/system/lib64/libyuv.so $NDK_LIB_PATH
+cp -f $PRODUCT_PATH/system/lib64/libyaml.so $NDK_LIB_PATH
+
+~/.local/bin/meson build --cross-file android-meson.cross --sysconfdir /vendor/etc/configs -Dpipelines=imx8-isi -Dandroid=enabled -Dandroid_root=$ANDROID_ROOT -Dtest=true --reconfigure
