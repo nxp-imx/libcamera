665b3be5e85ef547532cdbd047417fc1f23202dc
Fang Hui <hui.fang@nxp.com>
2023-12-27 22:33:14 +0800

    refine sequence in check-build.sh
    Signed-off-by: Fang Hui <hui.fang@nxp.com>

--- check-build.sh
+++ check-build.sh
@@ -1,4 +1,4 @@
 ls -l build/test/camera/capture
 ls -l build/src/libcamera/libcamera.so.0.1.0
-ls -l build/src/libcamera/base/libcamera-base.so.0.1.0
 ls -l build/src/android/libcamera-hal.so
+ls -l build/src/libcamera/base/libcamera-base.so.0.1.0
