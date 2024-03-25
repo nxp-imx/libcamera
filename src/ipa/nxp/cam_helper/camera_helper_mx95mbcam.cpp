f06737c3a3afcbac7fe767ffaeef48528c3b70d4
Julien Vuillaumier <julien.vuillaumier@nxp.com>
2024-03-25 19:56:25 +0100

    ipa: neo: camhelper: mx95mbcam: Add metadata support
    Linux drivers configures ox03c10 sensor with 2 top embedded data lines
    that report a number of specific registers, defined in the sensor init
    sequence.
    
    Metadata datagram for ox03c10 looks similar to the one used by ox05b10
    sensor. Thus, a metadata parser independent from ox0310 is created to
    enable possible reuse by other CameraHelper implementations.
    
    Actual registers list in embedded data is sensor-dependent so it has to
    be defined in the CameraHelper instance.
    
    Signed-off-by: Julien Vuillaumier <julien.vuillaumier@nxp.com>

--- src/ipa/nxp/cam_helper/camera_helper_mx95mbcam.cpp
+++ src/ipa/nxp/cam_helper/camera_helper_mx95mbcam.cpp
@@ -13,6 +13,9 @@
 #include <libcamera/base/log.h>
 
 #include "camera_helper.h"
+#include "md_parser_ox.h"
+
+#define ENABLE_EMBEDDED_DATA 1
 
 namespace libcamera {
 
@@ -20,13 +23,101 @@ LOG_DECLARE_CATEGORY(NxpCameraHelper)
 
 namespace nxp {
 
+/*
+ * Registers included in embedded data
+ * List comes from Linux driver init, defined by access control configuration
+ * for group 4 hold (register address 0x3804).
+ */
+constexpr uint32_t AecHcgCtrl01Reg = 0x3501;
+constexpr uint32_t AecHcgCtrl02Reg = 0x3502;
+constexpr uint32_t AecHcgCtrl08Reg = 0x3508;
+constexpr uint32_t AecHcgCtrl09Reg = 0x3509;
+constexpr uint32_t AecHcgCtrl0aReg = 0x350a;
+constexpr uint32_t AecHcgCtrl0bReg = 0x350b;
+constexpr uint32_t AecHcgCtrl0cReg = 0x350c;
+constexpr uint32_t AecSpdCtrl01Reg = 0x3541;
+constexpr uint32_t AecSpdCtrl02Reg = 0x3542;
+constexpr uint32_t AecSpdCtrl08Reg = 0x3548;
+constexpr uint32_t AecSpdCtrl09Reg = 0x3549;
+constexpr uint32_t AecSpdCtrl0aReg = 0x354a;
+constexpr uint32_t AecSpdCtrl0bReg = 0x354b;
+constexpr uint32_t AecSpdCtrl0cReg = 0x354c;
+constexpr uint32_t AecLcgCtrl08Reg = 0x3588;
+constexpr uint32_t AecLcgCtrl09Reg = 0x3589;
+constexpr uint32_t AecLcgCtrl0aReg = 0x358a;
+constexpr uint32_t AecLcgCtrl0bReg = 0x358b;
+constexpr uint32_t AecLcgCtrl0cReg = 0x358c;
+constexpr uint32_t AecVsCtrl01Reg = 0x35c1;
+constexpr uint32_t AecVsCtrl02Reg = 0x35c2;
+constexpr uint32_t AecVsCtrl08Reg = 0x35c8;
+constexpr uint32_t AecVsCtrl09Reg = 0x35c9;
+constexpr uint32_t AecVsCtrl0aReg = 0x35ca;
+constexpr uint32_t AecVsCtrl0bReg = 0x35cb;
+constexpr uint32_t AecVsCtrl0cReg = 0x35cc;
+constexpr uint32_t MipiCtrl3eReg = 0x483e;
+constexpr uint32_t MipiCtrl3fReg = 0x483f;
+constexpr uint32_t TmpReg26Reg = 0x4d2a;
+constexpr uint32_t TmpReg27Reg = 0x4d2b;
+constexpr uint32_t AwbGainHcg0Reg = 0x5280;
+constexpr uint32_t AwbGainHcg1Reg = 0x5281;
+constexpr uint32_t AwbGainHcg2Reg = 0x5282;
+constexpr uint32_t AwbGainHcg3Reg = 0x5283;
+constexpr uint32_t AwbGainHcg4Reg = 0x5284;
+constexpr uint32_t AwbGainHcg5Reg = 0x5285;
+constexpr uint32_t AwbGainHcg6Reg = 0x5286;
+constexpr uint32_t AwbGainHcg7Reg = 0x5287;
+constexpr uint32_t AwbGainLcg0Reg = 0x5480;
+constexpr uint32_t AwbGainLcg1Reg = 0x5481;
+constexpr uint32_t AwbGainLcg2Reg = 0x5482;
+constexpr uint32_t AwbGainLcg3Reg = 0x5483;
+constexpr uint32_t AwbGainLcg4Reg = 0x5484;
+constexpr uint32_t AwbGainLcg5Reg = 0x5485;
+constexpr uint32_t AwbGainLcg6Reg = 0x5486;
+constexpr uint32_t AwbGainLcg7Reg = 0x5487;
+constexpr uint32_t AwbGainSpd0Reg = 0x5680;
+constexpr uint32_t AwbGainSpd1Reg = 0x5681;
+constexpr uint32_t AwbGainSpd2Reg = 0x5682;
+constexpr uint32_t AwbGainSpd3Reg = 0x5683;
+constexpr uint32_t AwbGainSpd4Reg = 0x5684;
+constexpr uint32_t AwbGainSpd5Reg = 0x5685;
+constexpr uint32_t AwbGainSpd6Reg = 0x5686;
+constexpr uint32_t AwbGainSpd7Reg = 0x5687;
+constexpr uint32_t AwbGainVs0Reg = 0x5880;
+constexpr uint32_t AwbGainVs1Reg = 0x5881;
+constexpr uint32_t AwbGainVs2Reg = 0x5882;
+constexpr uint32_t AwbGainVs3Reg = 0x5883;
+constexpr uint32_t AwbGainVs4Reg = 0x5884;
+constexpr uint32_t AwbGainVs5Reg = 0x5885;
+constexpr uint32_t AwbGainVs6Reg = 0x5886;
+constexpr uint32_t AwbGainVs7Reg = 0x5887;
+
+constexpr std::initializer_list<uint32_t> registerList [[maybe_unused]] = {
+	AecHcgCtrl01Reg, AecHcgCtrl02Reg,
+	AecHcgCtrl08Reg, AecHcgCtrl09Reg, AecHcgCtrl0aReg, AecHcgCtrl0bReg,
+	AecHcgCtrl0cReg,
+	AecSpdCtrl01Reg, AecSpdCtrl02Reg,
+	AecSpdCtrl08Reg, AecSpdCtrl09Reg, AecSpdCtrl0aReg, AecSpdCtrl0bReg,
+	AecSpdCtrl0cReg,
+	AecLcgCtrl08Reg, AecLcgCtrl09Reg, AecLcgCtrl0aReg, AecLcgCtrl0bReg,
+	AecLcgCtrl0cReg,
+	AecVsCtrl01Reg, AecVsCtrl02Reg,
+	AecVsCtrl08Reg, AecVsCtrl09Reg, AecVsCtrl0aReg, AecVsCtrl0bReg, AecVsCtrl0cReg,
+	MipiCtrl3eReg, MipiCtrl3fReg,
+	TmpReg26Reg, TmpReg27Reg,
+	AwbGainHcg0Reg, AwbGainHcg1Reg, AwbGainHcg2Reg, AwbGainHcg3Reg,
+	AwbGainHcg4Reg, AwbGainHcg5Reg, AwbGainHcg6Reg, AwbGainHcg7Reg,
+	AwbGainLcg0Reg, AwbGainLcg1Reg, AwbGainLcg2Reg, AwbGainLcg3Reg,
+	AwbGainLcg4Reg, AwbGainLcg5Reg, AwbGainLcg6Reg, AwbGainLcg7Reg,
+	AwbGainSpd0Reg, AwbGainSpd1Reg, AwbGainSpd2Reg, AwbGainSpd3Reg,
+	AwbGainSpd4Reg, AwbGainSpd5Reg, AwbGainSpd6Reg, AwbGainSpd7Reg,
+	AwbGainVs0Reg, AwbGainVs1Reg, AwbGainVs2Reg, AwbGainVs3Reg,
+	AwbGainVs4Reg, AwbGainVs5Reg, AwbGainVs6Reg, AwbGainVs7Reg,
+};
+
 class CameraHelperMx95mbcam : public CameraHelper
 {
 public:
-	CameraHelperMx95mbcam()
-	{
-		/* gainType / gainConstants_ are unused */
-	}
+	CameraHelperMx95mbcam();
 
 	uint32_t gainCode(double gain) const override;
 	double gain(uint32_t gainCode) const override;
@@ -48,11 +139,44 @@ public:
 		uint32_t *maxGainCode, uint32_t *defGainCode = nullptr) const override;
 
 	std::map<int32_t, std::pair<uint32_t, bool>> delayedControlParams() const override;
+	void parseEmbedded(Span<const uint8_t> buffer, ControlList *mdControls) override;
 
 private:
 	/* total number of raws per frame from driver init sequence (unit double-raw) */
 	static constexpr unsigned int kVts = 0x2AEU;
+	std::unique_ptr<MdParser> parser_;
 };
+
+CameraHelperMx95mbcam::CameraHelperMx95mbcam()
+{
+	/*
+	 * Setup embedded data params
+	 * \todo setup actual min/max values
+	 */
+#if ENABLE_EMBEDDED_DATA
+	mdParams_.topLines = 2;
+#endif
+	int32_t minInt32 = std::numeric_limits<int32_t>::min();
+	int32_t maxInt32 = std::numeric_limits<int32_t>::max();
+
+	ControlInfoMap::Map ctrlMap = {};
+	ctrlMap.emplace(std::piecewise_construct,
+			std::forward_as_tuple(&md::AnalogueGain),
+			std::forward_as_tuple(minInt32, maxInt32));
+	ctrlMap.emplace(std::piecewise_construct,
+			std::forward_as_tuple(&md::DigitalGain),
+			std::forward_as_tuple(minInt32, maxInt32));
+	ctrlMap.emplace(std::piecewise_construct,
+			std::forward_as_tuple(&md::Exposure),
+			std::forward_as_tuple(minInt32, maxInt32));
+
+	mdParams_.controls =
+		ControlInfoMap(std::move(ctrlMap), md::controlIdMap);
+
+	parser_ = std::make_unique<MdParserOmniOx>(registerList);
+
+	/* Note: gainType / gainConstants_ are unused */
+}
 
 uint32_t CameraHelperMx95mbcam::gainCode(double gain) const
 {
@@ -174,7 +298,6 @@ void CameraHelperMx95mbcam::controlListSetExposure(
 	exposures.dcg = dcg;
 	exposures.spd = spd;
 	exposures.vs = vs;
-
 	Span<uint8_t> data(reinterpret_cast<uint8_t *>(&exposures),
 			   sizeof(ox03c10_exposure));
 
@@ -268,6 +391,58 @@ CameraHelperMx95mbcam::delayedControlParams() const
 	};
 
 	return params;
+}
+
+void CameraHelperMx95mbcam::parseEmbedded(Span<const uint8_t> buffer,
+					  ControlList *mdControls)
+{
+	(void)mdControls;
+
+	if (!buffer.size())
+		return;
+
+	MdParser::Status status;
+	MdParser::RegisterMap registers;
+	status = parser_->parse(buffer, registers);
+	if (status != MdParser::Status::OK)
+		return;
+
+	/*
+	 * Analog gain is currently configured identical for hcg, lcg, vs and
+	 * spd in controlListSetGain().
+	 * Registers mapping is derived from V4L2_CID_OX03C10_ANALOG_GAIN
+	 * implementation.
+	 */
+	uint32_t hcgAnalogGainCode;
+	hcgAnalogGainCode = ((registers[AecHcgCtrl08Reg] & 0xf) << 4) |
+			    ((registers[AecHcgCtrl09Reg] & 0xf0) >> 4);
+	mdControls->set(md::AnalogueGain, hcgAnalogGainCode);
+
+	/*
+	 * Digital gain is currently configured identical for hcg, lcg, vs and
+	 * spd in controlListSetGain().
+	 * Registers mapping is derived from V4L2_CID_OX03C10_DIGITAL_GAIN
+	 * implementation.
+	 */
+	uint32_t hcgDigitalGainCode;
+	hcgDigitalGainCode = ((registers[AecHcgCtrl0aReg] & 0xf) << 10) |
+			     ((registers[AecHcgCtrl0bReg] & 0xff) << 2) |
+			     ((registers[AecHcgCtrl0cReg] & 0xc0) >> 6);
+	mdControls->set(md::DigitalGain, hcgDigitalGainCode);
+
+	/*
+	 * Exposure is currently configured identical for dcg, vs and spd in
+	 * controlListSetExposure().
+	 * V4L2_CID_OX03C10_EXPOSURE to registers mapping comes from the
+	 * Linux driver implementation.
+	 * Sensor uses double row as exposure unit whereas standard
+	 * V4L2_CID_EXPOSURE control uses single row, hence the factor 2 here.
+	 */
+	uint32_t hcgExposure;
+	hcgExposure = ((registers[AecHcgCtrl01Reg] & 0xff) << 8) |
+		      ((registers[AecHcgCtrl02Reg] & 0xff));
+	hcgExposure *= 2;
+	mdControls->set(md::Exposure, hcgExposure);
 }
 
 REGISTER_CAMERA_HELPER("mx95mbcam", CameraHelperMx95mbcam)
