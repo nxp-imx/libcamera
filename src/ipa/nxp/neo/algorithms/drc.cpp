943e854455fdc0b9be96fd6b2083316f941eeb66
Celine Laurencin <celine.laurencin@nxp.com>
2024-07-02 11:57:01 +0200

    ipa: neo: drc: Apply the DRC global LUT values to all sensors.
    The DRC global LUT containing "universal" values is applied to all sensors.
    
    Signed-off-by: Celine Laurencin <celine.laurencin@nxp.com>

--- src/ipa/nxp/neo/algorithms/drc.cpp
+++ src/ipa/nxp/neo/algorithms/drc.cpp
@@ -26,7 +26,22 @@ namespace ipa::nxpneo::algorithms {
  * \brief DRC configuration
  *
  * This Algorithm configures the DRC unit.
- * It provides the static configuration for the global DRC look up table.
+ * It provides the static configuration for the global DRC operation.
+ *
+ * The DRC unit compresses the bit depth from 20 bit (used in the
+ * ISP pipeline) to 16 bits.
+ * The DRC operation controls the brightness of the output image.
+ *
+ * The DRC Global LUT (Tonemap) can be configured in yaml file using
+ * the "gbl-lut" attribute defining a list [1-416] of u16 values with 8.8 format.
+ * If no configuration is set, default values from the driver are used.
+ *
+ * The DRC Global gain is applied at the output of the global tonemapping step
+ * where all entries of the Global LUT are multiplied by this gain.
+ * It is used to compensate for the dark scene of an HDR sensor.
+ *
+ * The following DRC operation is applied on each YUV component.
+ * For the Y component: Y_GDRC = Y_IN * LUT[Linear2Bin(Y_IN)] * DRC_GBL_GAIN >> 4
  */
 
 LOG_DEFINE_CATEGORY(NxpNeoDrc)
