d1f5c67f2d3855c2187729384320ecb6f05dacab
Antoine Bouyer <antoine.bouyer@nxp.com>
2024-10-17 11:56:58 +0200

    Revert "ipa: libipa: Workaround build issue on Android related to duration division"
    This reverts commit 1aea44bad72091d562f8122fd499588a424623fb.

--- src/ipa/libipa/exposure_mode_helper.cpp
+++ src/ipa/libipa/exposure_mode_helper.cpp
@@ -184,22 +184,14 @@ ExposureModeHelper::splitExposure(utils::Duration exposure) const
 
 		if (stageShutter * lastStageGain >= exposure) {
 			shutter = clampShutter(exposure / clampGain(lastStageGain));
-#ifdef ANDROID
-			gain = clampGain(exposure.get<std::nano>() / shutter.get<std::nano>());
-#else
 			gain = clampGain(exposure / shutter);
-#endif
 
 			return { shutter, gain, exposure / (shutter * gain) };
 		}
 
 		if (stageShutter * stageGain >= exposure) {
 			shutter = clampShutter(exposure / clampGain(stageGain));
-#ifdef ANDROID
-			gain = clampGain(exposure.get<std::nano>() / shutter.get<std::nano>());
-#else
 			gain = clampGain(exposure / shutter);
-#endif
 
 			return { shutter, gain, exposure / (shutter * gain) };
 		}
@@ -213,11 +205,7 @@ ExposureModeHelper::splitExposure(utils::Duration exposure) const
 	 * shutter time is maxed before gain is touched at all.
 	 */
 	shutter = clampShutter(exposure / clampGain(stageGain));
-#ifdef ANDROID
-	gain = clampGain(exposure.get<std::nano>() / shutter.get<std::nano>());
-#else
 	gain = clampGain(exposure / shutter);
-#endif
 
 	return { shutter, gain, exposure / (shutter * gain) };
 }
